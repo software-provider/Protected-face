35be25feb4451a7368e40f4e43b83991204a7721
Software Provider <whitesnow3602@gmail.com>
2023-12-02 01:06:02 +0900

    Update seg_tflite.py

--- coco models/tflite mobnetv1 ssd/seg_tflite.py
+++ coco models/tflite mobnetv1 ssd/seg_tflite.py
@@ -1,30 +1,10 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Aug 18 16:23:16 2020
-
-@author: hp
-"""
-
 import numpy as np
 import tensorflow as tf
 import cv2
 import visualization_utils as vis_util
 
 def create_category_index(label_path='coco_ssd_mobilenet/labelmap.txt'):
-    """
-    To create dictionary of label map
-
-    Parameters
-    ----------
-    label_path : string, optional
-        Path to labelmap.txt. The default is 'coco_ssd_mobilenet/labelmap.txt'.
-
-    Returns
-    -------
-    category_index : dict
-        nested dictionary of labels.
-
-    """
+   
     f = open(label_path)
     category_index = {}
     for i, val in enumerate(f):
@@ -36,31 +16,7 @@ def create_category_index(label_path='coco_ssd_mobilenet/labelmap.txt'):
     f.close()
     return category_index
 def get_output_dict(image, interpreter, output_details, nms=True, iou_thresh=0.5, score_thresh=0.6):
-    """
-    Function to make predictions and generate dictionary of output
-
-    Parameters
-    ----------
-    image : Array of uint8
-        Preprocessed Image to perform prediction on
-    interpreter : tensorflow.lite.python.interpreter.Interpreter
-        tflite model interpreter
-    input_details : list
-        input details of interpreter
-    output_details : list
-    nms : bool, optional
-        To perform non-maximum suppression or not. The default is True.
-    iou_thresh : int, optional
-        Intersection Over Union Threshold. The default is 0.5.
-    score_thresh : int, optional
-        score above predicted class is accepted. The default is 0.6.
-
-    Returns
-    -------
-    output_dict : dict
-        Dictionary containing bounding boxes, classes and scores.
-
-    """
+    
     output_dict = {
                    'detection_boxes' : interpreter.get_tensor(output_details[0]['index'])[0],
                    'detection_classes' : interpreter.get_tensor(output_details[1]['index'])[0],
@@ -74,32 +30,7 @@ def get_output_dict(image, interpreter, output_details, nms=True, iou_thresh=0.5
     return output_dict
 
 def apply_nms(output_dict, iou_thresh=0.5, score_thresh=0.6):
-    """
-    Function to apply non-maximum suppression on different classes
-
-    Parameters
-    ----------
-    output_dict : dictionary
-        dictionary containing:
-            'detection_boxes' : Bounding boxes coordinates. Shape (N, 4)
-            'detection_classes' : Class indices detected. Shape (N)
-            'detection_scores' : Shape (N)
-            'num_detections' : Total number of detections i.e. N. Shape (1)
-    iou_thresh : int, optional
-        Intersection Over Union threshold value. The default is 0.5.
-    score_thresh : int, optional
-        Score threshold value below which to ignore. The default is 0.6.
-
-    Returns
-    -------
-    output_dict : dictionary
-        dictionary containing only scores and IOU greater than threshold.
-            'detection_boxes' : Bounding boxes coordinates. Shape (N2, 4)
-            'detection_classes' : Class indices detected. Shape (N2)
-            'detection_scores' : Shape (N2)
-            where N2 is the number of valid predictions after those conditions.
-
-    """
+   
     q = 90 # no of classes
     num = int(output_dict['num_detections'])
     boxes = np.zeros([1, num, q, 4])
@@ -126,32 +57,7 @@ def apply_nms(output_dict, iou_thresh=0.5, score_thresh=0.6):
     return output_dict
 
 def make_and_show_inference(img, interpreter, input_details, output_details, category_index, nms=True, score_thresh=0.6, iou_thresh=0.5):
-    """
-    Generate and draw inference on image
-
-    Parameters
-    ----------
-    img : Array of uint8
-        Original Image to find predictions on.
-    interpreter : tensorflow.lite.python.interpreter.Interpreter
-        tflite model interpreter
-    input_details : list
-        input details of interpreter
-    output_details : list
-        output details of interpreter
-    category_index : dict
-        dictionary of labels
-    nms : bool, optional
-        To perform non-maximum suppression or not. The default is True.
-    score_thresh : int, optional
-        score above predicted class is accepted. The default is 0.6.
-    iou_thresh : int, optional
-        Intersection Over Union Threshold. The default is 0.5.
-
-    Returns
-    -------
-    NONE
-    """
+   
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img_rgb = cv2.resize(img_rgb, (300, 300), cv2.INTER_AREA)
     img_rgb = img_rgb.reshape([1, 300, 300, 3])
